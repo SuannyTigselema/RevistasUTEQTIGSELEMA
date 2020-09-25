cfe09da65384b8a7c7ee7b37da6cc07fea257f88
ingrid <ingrid.ortiz2016@uteq.edu.ec>
2020-09-25 03:15:23 -0500

    cambio de pantalla de inicio

--- app/src/main/java/com/example/revistasuteq/MainActivity.java
+++ app/src/main/java/com/example/revistasuteq/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        // Agregar animaciones
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, activityPrincipal.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+
+
+        //En caso de que quieran hacer con el bton
         btnContinuar=(Button) findViewById(R.id.btnContinuar);
         btnContinuar.setOnClickListener(new View.OnClickListener() {
             @Override
