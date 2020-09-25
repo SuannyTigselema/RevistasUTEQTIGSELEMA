38a58c7f15c8089ab19fb6264d0b9fdb77d36651
Paola Morales <PaolaSMorales@users.noreply.github.com>
2020-09-25 16:16:38 -0500

    Botones para suscribirse al artículo

--- app/src/main/java/com/example/revistasuteq/activity_detalle_articulo.java
+++ app/src/main/java/com/example/revistasuteq/activity_detalle_articulo.java
@@ -14,19 +14,21 @@ import android.widget.Toast;
 import com.example.revistasuteq.objetos.articulo;
 
 public class activity_detalle_articulo extends AppCompatActivity {
-    articulo  art_selec;
-    TextView txtTitulo,txtDOI,txtPalabrasClave,txtResumen,txtAutores;
-    Button btnVer;
+    articulo art_selec;
+    TextView txtTitulo, txtDOI, txtPalabrasClave, txtResumen, txtAutores;
+    Button btnVer, btnSuscribirse_Detalle;
+    int imgResource;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_articulo);
-        txtTitulo=findViewById(R.id.txtTituloDA);
-        txtDOI=findViewById(R.id.txtDoiAD);
-        txtAutores=findViewById(R.id.txtAutores);
-        txtPalabrasClave=findViewById(R.id.txtpalabrasclaveAD);
-        txtResumen=findViewById(R.id.txtResumenAD);
-        btnVer=findViewById(R.id.btnVer);
+        btnSuscribirse_Detalle = findViewById(R.id.btnNotificar_Articulo_Detalle);
+        txtTitulo = findViewById(R.id.txtTituloDA);
+        txtDOI = findViewById(R.id.txtDoiAD);
+        txtAutores = findViewById(R.id.txtAutores);
+        txtPalabrasClave = findViewById(R.id.txtpalabrasclaveAD);
+        txtResumen = findViewById(R.id.txtResumenAD);
+        btnVer = findViewById(R.id.btnVer);
         btnVer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,41 +36,51 @@ public class activity_detalle_articulo extends AppCompatActivity {
             }
         });
 
+        //Preguntar si está suscrito o no
+        //------------------------------------------------------------------------------------------------------
+        imgResource = R.drawable.icon_suscrito;
+        btnSuscribirse_Detalle.setCompoundDrawablesWithIntrinsicBounds(imgResource, 0, 0, 0);
+        btnSuscribirse_Detalle.setText("Suscrito");
+        //------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------
+        imgResource = R.drawable.icon_no_suscrito_blanco;
+        btnSuscribirse_Detalle.setCompoundDrawablesWithIntrinsicBounds(imgResource, 0, 0, 0);
+        btnSuscribirse_Detalle.setText("Suscribirse");
+        //------------------------------------------------------------------------------------------------------
+
+
         art_selec = (articulo) getIntent().getSerializableExtra("articulo");
         txtTitulo.setText(art_selec.getTitulo());
         txtDOI.setText(art_selec.getDoi());
         txtResumen.setText(Html.fromHtml(art_selec.getResumen()));
 
-      //  txtResumen.setText(art_selec.toString());
-        String keys=" ";
-        for (int i =0; i<art_selec.getLstKeywordss().size();i++)
-        {
-            if(i==0)
-                keys=art_selec.getLstKeywordss().get(i).getKeyword()+".";
+        //  txtResumen.setText(art_selec.toString());
+        String keys = " ";
+        for (int i = 0; i < art_selec.getLstKeywordss().size(); i++) {
+            if (i == 0)
+                keys = art_selec.getLstKeywordss().get(i).getKeyword() + ".";
             else
-                keys=art_selec.getLstKeywordss().get(i).getKeyword()+","+keys;
+                keys = art_selec.getLstKeywordss().get(i).getKeyword() + "," + keys;
         }
         txtPalabrasClave.setText(keys);
-        String autores=" ";
-        for (int i =0; i<art_selec.getLstAutores().size();i++)
-        {
+        String autores = " ";
+        for (int i = 0; i < art_selec.getLstAutores().size(); i++) {
             //autores=art_selec.getLstAutores().get(i).getNombres()+"-"+art_selec.getLstAutores().get(i).getFiliacion()+"\n"+autores;
-            autores=art_selec.getLstAutores().get(i).getNombres()+"\n"+autores;
+            autores = art_selec.getLstAutores().get(i).getNombres() + "\n" + autores;
         }
         txtAutores.setText(autores);
-       // Bundle b = this.getIntent().getExtras();
+        // Bundle b = this.getIntent().getExtras();
         //String articuloID=b.getString("articuloID");
        /* Toast toast1=Toast.makeText(getApplicationContext(),
                 "Seleccionaste: " + art_selec.getFecha_publicacion(), Toast.LENGTH_SHORT);
         toast1.show();*/
 
-        txtDOI.setOnClickListener(new View.OnClickListener()
-        {
+        txtDOI.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Uri uri= Uri.parse(txtDOI.getText().toString());
-                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                Uri uri = Uri.parse(txtDOI.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
         });
@@ -80,5 +92,20 @@ public class activity_detalle_articulo extends AppCompatActivity {
 
         startActivity(intent);
         this.finish();
+    }
+
+    public void suscribirse(View view)
+    {
+        //Si se suscribió cambiar ícono e imagen
+        int imgResource = R.drawable.icon_suscrito;
+        btnSuscribirse_Detalle.getResources();
+        btnSuscribirse_Detalle.setCompoundDrawablesWithIntrinsicBounds(imgResource, 0, 0, 0);
+        btnSuscribirse_Detalle.setText("Suscrito");
+
+//        //Si dejó de suscribirse utilizar esta imagen
+//        imgResource = R.drawable.icon_no_suscrito_blanco;
+//        btnSuscribirse_Detalle.getResources();
+//        btnSuscribirse_Detalle.setCompoundDrawablesWithIntrinsicBounds(imgResource, 0, 0, 0);
+//        btnSuscribirse_Detalle.setText("Suscribirse");
     }
 }
--- app/src/main/java/com/example/revistasuteq/adaptadores/adpArticulo.java
+++ app/src/main/java/com/example/revistasuteq/adaptadores/adpArticulo.java
@@ -1,10 +1,20 @@
 package com.example.revistasuteq.adaptadores;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,21 +24,21 @@ import com.example.revistasuteq.objetos.articulo;
 
 import java.util.ArrayList;
 
-public class adpArticulo extends RecyclerView.Adapter<adpArticulo.MyViewHolder>  implements View.OnClickListener{
+public class adpArticulo extends RecyclerView.Adapter<adpArticulo.MyViewHolder> implements View.OnClickListener {
 
     ArrayList<articulo> arrayListMember;
 
     private View.OnClickListener listener;
-    adpArticulo( ArrayList<articulo> arrayListMember)
-    {
-        this.arrayListMember=arrayListMember;
+
+    adpArticulo(ArrayList<articulo> arrayListMember) {
+        this.arrayListMember = arrayListMember;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_articulos,parent,false);
+                .inflate(R.layout.list_articulos, parent, false);
         view.setOnClickListener(this);
         return new adpArticulo.MyViewHolder(view);
     }
@@ -37,13 +47,21 @@ public class adpArticulo extends RecyclerView.Adapter<adpArticulo.MyViewHolder> 
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.txtNombreArticulo.setText(arrayListMember.get(position).getTitulo());
         holder.txtFechaArticulo.setText(arrayListMember.get(position).getFecha_publicacion());
-     /*   ArrayList<String> arrayListMember=new ArrayList<>();
-        for (int i=1;i<=4;i++)
-        {
-            arrayListMember.add("Member"+i);
-        }
-        adpArticulo adapterMember=new adpArticulo(arrayListMember);
-        */
+
+        //Preguntar si está suscrito o no
+        //Si esta suscrito el tag es "noti_si"
+        //Si no lo está el tag es "noti_no"
+        //---------------------------------------------------------------------
+        holder.btnActivarNotificar.setTag("noti_no");
+        //---------------------------------------------------------------------
+        if(holder.btnActivarNotificar.getTag().equals("noti_si"))
+            holder.btnActivarNotificar.setImageResource(R.drawable.icon_notificar_activo);
+        else
+            holder.btnActivarNotificar.setImageResource(R.drawable.icon_notificar_no);
+        //---------------------------------------------------------------------
+
+        holder.setOnClickListeners();
+
     }
 
     @Override
@@ -53,23 +71,54 @@ public class adpArticulo extends RecyclerView.Adapter<adpArticulo.MyViewHolder> 
 
     @Override
     public void onClick(View view) {
-        if(listener!=null)
-        {
+        if (listener != null) {
             listener.onClick(view);
         }
     }
 
-    public void setOnClickListener(View.OnClickListener listener){
-        this.listener=listener;
+    public void setOnClickListener(View.OnClickListener listener) {
+        this.listener = listener;
     }
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         int view_type;
         TextView txtNombreArticulo;
         TextView txtFechaArticulo;
+        ImageView btnActivarNotificar;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            txtNombreArticulo=itemView.findViewById(R.id.txtnombreArticulo);
-            txtFechaArticulo=itemView.findViewById(R.id.txtfechaArticulo);
+            txtNombreArticulo = itemView.findViewById(R.id.txtnombreArticulo);
+            txtFechaArticulo = itemView.findViewById(R.id.txtfechaArticulo);
+            btnActivarNotificar = itemView.findViewById(R.id.btnNotificar_Articulo);
+
+        }
+
+        public void setOnClickListeners() {
+            btnActivarNotificar.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            if (btnActivarNotificar.getTag().equals("noti_no")) {
+                //Cuando se suscribe
+                btnActivarNotificar.setImageResource(R.drawable.icon_notificar_activo);
+                btnActivarNotificar.setTag("noti_si");
+            } else {
+                //Cuando deja de suscribirse
+                btnActivarNotificar.setImageResource(R.drawable.icon_notificar_no);
+                btnActivarNotificar.setTag("noti_no");
+            }
+            //Obtener el id de la publicación
+            Toast.makeText(view.getContext(), "Selección " + arrayListMember.get(getAdapterPosition()).getPublicacion_id(), Toast.LENGTH_SHORT).show();
+
+//            Intent intent = new Intent(view.getContext(), PDF.class);
+//            Bundle b = new Bundle();
+//            b.putString("url", items.get(getAdapterPosition()).getGaleys());
+//
+//            intent.putExtras(b);
+//            view.getContext().startActivity(intent);
         }
     }
 }
